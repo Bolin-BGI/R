e30e9af9d953b4c379edc2d0dd720808b9e530b1
Bolin <113431520+Bolin-BGI@users.noreply.github.com>
2025-04-23 19:29:15 +0800

    GO_KEGG.R

--- GO_KEGG.R
+++ GO_KEGG.R
@@ -5,15 +5,24 @@ library(ggplot2)
 library(GOplot)
 
 
-
 # 读取CSV文件，去除 'p-' 'm-' 前缀
-marker <- read.csv("./YWHAG_markers.csv")
-colnames(marker)[1] <- "gene" # 将第一列命名为'gene'
+marker <- read.csv("./kc_celltype_findmarkers.csv",row.names = 1)
+#colnames(marker)[1] <- "geneID" # 将第一列命名为'gene'
 
+unique(marker$cluster)
+# [1] "KC-migratory"         "Activated KC_spinous"
+# [3] "TAC"                  "KC_basal_prolif"     
+# [5] "KC-HS"                "KC-spinous"          
+# [7] "KC_Stress-Responsive" "KC-basal"            
+# [9] "KC_ACTA2"             "KC-IRS"              
+# [11] "SAC"                  "KC-ORS
+
+name = 'KC_Stress-Responsive' # 'KC-migratory'  'Activated KC_spinous' 'KC_basal_prolif' 'KC_Stress-Responsive'
 
 # 筛选上调表达的基因并选择前 100个基因
-top_up <- marker %>% filter(avg_log2FC > 0.3 & p_val < 0.05) %>% arrange(desc(avg_log2FC), p_val_adj) %>% 
-          filter(!grepl("^ENSSS", gene) & !grepl("^ENSMFA", gene) & !grepl("^(RPL|RPS)", gene)) # %>% head(100)
+top_up <- marker %>% filter(cluster == name) %>% filter(avg_log2FC > 0.5 & p_val < 0.05) %>% arrange(desc(avg_log2FC), p_val_adj) %>% 
+  filter(!grepl("^ENSSS", gene) & !grepl("^ENSMFA", gene) & !grepl("^(RPL|RPS)", gene)) # %>% head(100)
+nrow(top_up)
 
 # 转换基因符号为ENTREZID
 gene_ID <- bitr(top_up$gene, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
@@ -33,16 +42,16 @@ if (qvalueFilter > 0.05) {
 
 # main
 GO <- enrichGO(gene = gene_ID$ENTREZID,OrgDb = org.Hs.eg.db,
-              keyType = "ENTREZID",  # 记得和输入基因的格式保持一致
-              ont = "ALL", # BP(Biological Process)  MF(Molecular Function)  CC(Cellular Component) 
-              pvalueCutoff = 0.5, 
-              qvalueCutoff = 0.5,
-              readable = TRUE)
+               keyType = "ENTREZID",  # 记得和输入基因的格式保持一致
+               ont = "ALL", # BP(Biological Process)  MF(Molecular Function)  CC(Cellular Component) 
+               pvalueCutoff = 0.5, 
+               qvalueCutoff = 0.5,
+               readable = TRUE)
+print(paste0('The number of GO_items is : ',nrow(GO)))
 
-
-df = as.data.frame(GO)
-df_ed = df[(df$pvalue < pvalueFilter & df$qvalue < qvalueFilter),] 
-write.csv(df_ed, file = "YWHAG_positive_GO_up.csv", row.names = FALSE)
+# df = as.data.frame(GO)
+# df_ed = df[(df$pvalue < pvalueFilter & df$qvalue < qvalueFilter),] 
+# write.csv(df_ed, file = "YWHAG_positive_GO_up.csv", row.names = FALSE)
 
 # 设置显示的条目数
 showNum = 10
@@ -53,11 +62,12 @@ if (nrow(GO) < 10) {
 # 如果有富集结果，生成气泡图
 if (nrow(GO) != 0) {
   # 生成气泡图
-  pdf(file = paste0(name, "_GO.pdf"), width = 10, height = 15)
+  pdf(file = paste0(name, "_GO.pdf"), width = 12, height = 15)
   p1 <- dotplot(GO,x = "GeneRatio", color = colorSel, size = "Count", showCategory = showNum,label_format=150,split="ONTOLOGY") + # 以 ONTOLOGY 类型分开  
     facet_grid(ONTOLOGY~., scales = 'free') # 以 ONTOLOGY 类型分屏绘图
   print(p1)
   #dev.off()
+  
   
   # 生成条形图
   #pdf(file = "GO_bar.pdf", width = 10, height = 8)
@@ -75,8 +85,8 @@ if (nrow(GO) != 0) {
 
 
 
-
 # ----------------------------------- KEGG ----------------------------------
+
 
 # 设置p值和q值过滤阈值
 pvalueFilter = 0.2 # 0.05  
@@ -88,25 +98,36 @@ if (qvalueFilter > 0.05) {
   colorSel = "pvalue"
 }
 
+
 # 筛选上调表达的基因并选择前 100个基因
-top_up <- marker %>% filter(avg_log2FC > 0.3 & p_val_adj < 0.05) %>% arrange(desc(avg_log2FC), p_val_adj) 
+top_up <- marker %>% filter(cluster == name) %>% 
+          filter(avg_log2FC > 0.3 & p_val_adj < 0.05) %>% arrange(desc(avg_log2FC), p_val_adj) 
+nrow(top_up)
+
 # 筛选下调表达的基因并选择前 100 个基因
-top_down <- marker %>% filter(avg_log2FC < 0.3 & p_val_adj < 0.05) %>% arrange(avg_log2FC, p_val_adj) #%>% head(100)
+top_down <- marker %>% filter(cluster == name) %>% 
+          filter(avg_log2FC < 0.3 & p_val_adj < 0.05) %>% arrange(avg_log2FC, p_val_adj) #%>% head(100)
+
+nrow(top_down)
 
 # 转换基因符号为ENTREZID
 gene_ID_up <- bitr(top_up$gene, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db) 
 gene_ID_down <- bitr(top_down$gene, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
 
+
 # KEGG富集分析
 KEGG_up <- enrichKEGG(gene = gene_ID_up$ENTREZID, organism = "hsa", pvalueCutoff = pvalueFilter, qvalueCutoff = qvalueFilter)
+print(paste0('The number of KEGG_up is : ',nrow(KEGG_up)))
+
 KEGG_down <- enrichKEGG(gene = gene_ID_down$ENTREZID, organism = "hsa", pvalueCutoff = pvalueFilter, qvalueCutoff = qvalueFilter)
+print(paste0('The number of KEGG_down is : ',nrow(KEGG_down)))
 
 
 
 ## Plots
 if (nrow(KEGG_up) != 0) {
   
-  pdf(file = paste0(name,"_KEGG_up.pdf"), width = 10, height = 10)
+  pdf(file = paste0(name,"_KEGG_up.pdf"), width = 12, height = 10)
   
   p1 <- barplot(KEGG_up, x = "Count", color = colorSel, showCategory = 10, font.size = 15, title = "KEGG enrichment barplot", label_format = 50) # 超过40个字符串换行
   print(p1)
@@ -123,9 +144,9 @@ if (nrow(KEGG_up) != 0) {
 
 
 ## Plots
-if (nrow(KEGG_down) != 0) {
+if (nrow(KEGG_down) != 0 ) {
   
-  pdf(file = paste0(name,"_KEGG_down.pdf"), width = 10, height = 10)
+  pdf(file = paste0(name,"_KEGG_down.pdf"), width = 12, height = 10)
   
   p1 <- barplot(KEGG_down, x = "Count", color = colorSel, showCategory = 10, font.size = 15, title = "KEGG enrichment barplot", label_format = 50) # 超过40个字符串换行
   print(p1)
@@ -142,16 +163,18 @@ if (nrow(KEGG_down) != 0) {
 
 
 
-df = as.data.frame(KEGG_up)
-df$geneID = as.character(sapply(df$geneID, function(x) paste(gene_ID$SYMBOL[match(strsplit(x, "/")[[1]], as.character(gene_ID$ENTREZID))], collapse = "/"))) # 将geneID转换为基因符号
-df_ed = df[(df$pvalue < pvalueFilter & df$qvalue < qvalueFilter),] 
-write.csv(df_ed, file = "YWHAG_KEGG_up.csv", row.names = FALSE)
+# df = as.data.frame(KEGG_up)
+# df$geneID = as.character(sapply(df$geneID, function(x) paste(gene_ID$SYMBOL[match(strsplit(x, "/")[[1]], as.character(gene_ID$ENTREZID))], collapse = "/"))) # 将geneID转换为基因符号
+# df_ed = df[(df$pvalue < pvalueFilter & df$qvalue < qvalueFilter),] 
+# write.csv(df_ed, file = "YWHAG_KEGG_up.csv", row.names = FALSE)
+# 
+# 
+# df = as.data.frame(KEGG_down)
+# df$geneID = as.character(sapply(df$geneID, function(x) paste(gene_ID$SYMBOL[match(strsplit(x, "/")[[1]], as.character(gene_ID$ENTREZID))], collapse = "/"))) # 将geneID转换为基因符号
+# df_ed = df[(df$pvalue < pvalueFilter & df$qvalue < qvalueFilter),] 
+# write.csv(df_ed, file = "YWHAG_KEGG_down.csv", row.names = FALSE)
 
 
-df = as.data.frame(KEGG_down)
-df$geneID = as.character(sapply(df$geneID, function(x) paste(gene_ID$SYMBOL[match(strsplit(x, "/")[[1]], as.character(gene_ID$ENTREZID))], collapse = "/"))) # 将geneID转换为基因符号
-df_ed = df[(df$pvalue < pvalueFilter & df$qvalue < qvalueFilter),] 
-write.csv(df_ed, file = "YWHAG_KEGG_down.csv", row.names = FALSE)
 
 
 
